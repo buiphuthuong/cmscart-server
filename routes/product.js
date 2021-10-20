069f9b7281e5dc4b0d3a222bbf3faea5f799b960
ThuongBui <buiphuthuong@gmail.com>
2021-10-20 22:36:48 +0700

    first commit

--- routes/product.js
+++ routes/product.js
@@ -46,7 +46,7 @@ router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
 //GET PRODUCT BY SKU
 router.get('/sku/:sku', async (req, res) => {
   try {
-    const product = await Product.findOne({ sku: req.params.sku }).exec()
+    const product = await Product.find({ sku: req.params.sku }).exec()
     console.log(product)
     console.log(req.params.sku)
     res.status(200).json(product)
